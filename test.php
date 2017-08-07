4435dafcb5476c89cc6d55d1701d36b7b1a88193
Juanjo <calambre75@gmail.com>
2017-08-07 07:45:48 +0200

    Añadido TEST2

--- test.php
+++ test.php
@@ -2,12 +2,18 @@
 define( 'FORMAT1', [
   'uid' => SQLITE3_INTEGER,
   'name' => [ SQLITE3_TEXT, NULL, SQLITE3_NULL ],
-  'state' => 
+  'state' => [ SQLITE3_INTEGER, 1 ],
+  'pin' => SQLITE3_INTEGER,
+  'notes' => [ SQLITE3_TEXT, NULL, SQLITE3_NULL ]
 ] );
 define( 'DATA1', [
   'uid' => 1,
+  'name' => 'Juanjo'
+] );
+define( 'DATA2', [
+  'uid' => 1,
   'name' => 'Juanjo',
-  'created' => 0
+  'pin' => 0
 ] );
 
 require( './dbrowformat.php' );
@@ -22,7 +28,36 @@ class Statement {
   }
 }
 
-$test = new DBRowFormat( FORMAT1, DATA1, DBRowFormat::REQUIREALL, TRUE );
+try {
+  $error1 = NULL;
+  $test1 = new DBRowFormat( FORMAT1, DATA1, DBRowFormat::REQUIREALL, TRUE );
+} catch( Exception $err ) {
+  $error1 = $err->getMessage( );
+}
 
-echo 'names: ', $test->names( ), "\n";
-echo 'params: ', $test->params( ), "\n";
+echo "TEST 1:\n";
+
+if( empty( $error1 ) ){
+  echo "FALLIDO !!\n";
+  exit( );
+} else {
+  echo "PASADO\n";
+  echo 'Error generado: ' . $error1 . "\n";
+}
+
+echo "\nTEST 2:";
+
+try {
+  $error2 = NULL;
+  $test2 = new DBRowFormat( FORMAT1, DATA2, DBRowFormat::REQUIREALL, FALSE );
+} catch( Exception $err ) {
+  $error2 = $err->getMessage( );
+}
+
+if( empty( $error2 ) ) {
+  echo "PASADO.\n";
+  echo 'names->', $test2->names( TRUE ), "\n";
+  echo 'params->', $test2->params( TRUE ), "\n";
+} else {
+  echo "FALLIDO !!\nError generado: " . $error2 . "\n";
+}
